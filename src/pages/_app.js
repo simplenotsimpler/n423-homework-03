335e1369f82a459a7b7ca380fa0b35490e907197
SimpleNotSimpler <simplenotsimpler@outlook.com>
2023-04-21 02:36:55 -0400

    fix: general styling and tweaks

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,31 @@
+/* 
+
+  using next/font
+  https://blog.logrocket.com/next-js-font-optimization-custom-google-fonts/
+  https://dev.to/mikeesto/how-to-use-nextfont-globally-4img
+*/
+
 import Layout from "@/components/Layout.jsx";
 import "@/styles/globals.css";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  display: "swap",
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+});
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <style jsx global>{`
+        :root {
+          --roboto-font: ${roboto.style.fontFamily};
+        }
+      `}</style>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,12 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import Login from "@/components/Login.jsx";
 import { useState } from "react";
 import useFirebase from "@/hooks/useFirebase.js";
 import PostsList from "@/components/PostsList.jsx";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   const firebase = useFirebase();
   const [postsList, setPostsList] = useState([]);
-
 
   //TODO: Move this up to a context so gets pulled automatically
   async function pullPostsFromDb() {
@@ -23,7 +17,6 @@ export default function Home() {
     <>
       {firebase.currentUser.email ? (
         <>
-          <p>Welcome!! {firebase.currentUser.displayName}</p>
           <button onClick={pullPostsFromDb}>Get Posts</button>
           <PostsList posts={postsList} />
         </>
